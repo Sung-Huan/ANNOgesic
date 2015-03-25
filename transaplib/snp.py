11c88a8ecb75e2ecac02a7cc85d53f8800b4e919
sunghuan <sung-huan.yu@uni-wuerzburg.de>
2015-03-25 16:45:54 +0100

    update snp

--- transaplib/snp.py
+++ transaplib/snp.py
@@ -23,19 +23,19 @@ class SNP_calling(object):
                 bams.append(bam_folder + "/" + bam)
         return num_bam
 
-    def _transcript_SNP(self, fasta, snp, out_table_prefix, 
-                        qual, seq_path, fasta_prefix, read_depth, 
-                        stat_path, types, bam_number, fraction):
-        stat_file = os.path(stat_path, 
-                   "".join(["stat_", fasta_prefix, types, "_SNP.csv"]))
+    def _transcript_SNP(self, fasta, snp, out_table_prefix, qual, 
+                        seq_path, fasta_prefix, read_depth, stat_path, 
+                        types, bam_number, fraction, table_path):
+        stat_file = os.path.join(stat_path, 
+                   "_".join(["stat", fasta_prefix + types, "SNP.csv"]))
         out_stat = open(stat_file, "w")
         SNP_detect(fasta, snp, out_table_prefix, qual, seq_path + fasta_prefix, 
                    read_depth, fraction, bam_number, stat_file)
-        self.helper.move_all_content(out_table_prefix, stat_path, ".png") 
+        self.helper.move_all_content(table_path, stat_path, [".png"]) 
 
     def _run_program(self, program, samtools_path, bcftools_path, fasta_file, out_bcf, 
                      out_raw_prefix, out_table_prefix, quality, seq_path, prefix, 
-                     depth, stat_path, bam_number, fraction, out_folder):
+                     depth, stat_path, bam_number, fraction, out_folder, table_path):
         if "1" in program:
             print("Running SNP calling with BAQ...")
             call([samtools_path, "mpileup", 
@@ -49,7 +49,7 @@ class SNP_calling(object):
                                  out_table_prefix + "_with_BAQ", quality,
                                  os.path.join(seq_path, "with_BAQ", prefix),
                                  prefix, depth, stat_path, "_with_BAQ",
-                                 bam_number, fraction)
+                                 bam_number, fraction, table_path)
         if "2" in program:
             print("Running SNP calling without BAQ...")
             call([samtools_path, "mpileup", 
@@ -63,7 +63,7 @@ class SNP_calling(object):
                                  out_table_prefix + "_without_BAQ", quality,
                                  os.path.join(seq_path, "without_BAQ", prefix),
                                  prefix, depth, stat_path, "_without_BAQ",
-                                 bam_number, fraction)
+                                 bam_number, fraction, table_path)
         if "3" in program:
             print("Running SNP calling extend BAQ...")
             call([samtools_path, "mpileup",
@@ -77,7 +77,7 @@ class SNP_calling(object):
                                  out_table_prefix + "_extend_BAQ", quality,
                                  os.path.join(seq_path, "extend_BAQ", prefix),
                                  prefix, depth, stat_path, "_extend_BAQ",
-                                 bam_number, fraction)
+                                 bam_number, fraction, table_path)
 
     def _detect_fasta(self, fasta):
         detect = False
@@ -93,8 +93,8 @@ class SNP_calling(object):
         return (detect, prefix)
 
     def _merge_bams(self, normal_bams, frag_bams, samtools_path, out_folder):
-        if "whole_reads.bam" in os.listdir(out_folder):
-            self.helper.remove_all_content(out_folder, "whole_read", "file")
+#        if "whole_reads.bam" in os.listdir(out_folder):
+#            self.helper.remove_all_content(out_folder, "whole_read", "file")
         bams = []
         num_normal = 0
         num_frag = 0
@@ -109,14 +109,14 @@ class SNP_calling(object):
                 num_frag = self._import_bam(frag_bams, bams)
         num_bam = num_normal + num_frag
         print("Merge BAM files now ...")
-        command = (" ".join([samtools_path, "merge", 
-                   os.path.join(out_folder, "whole_reads.bam"), " ".join(bams)]))
-        os.system(command)
+#        command = (" ".join([samtools_path, "merge", 
+#                   os.path.join(out_folder, "whole_reads.bam"), " ".join(bams)]))
+#        os.system(command)
         print("Sort BAM file now ...")
-        command = (" ".join([samtools_path, "sort", 
-                   os.path.join(out_folder, "whole_reads.bam"),
-                   os.path.join(out_folder, "whole_reads_sorted")]))
-        os.system(command)
+#        command = (" ".join([samtools_path, "sort", 
+#                   os.path.join(out_folder, "whole_reads.bam"),
+#                   os.path.join(out_folder, "whole_reads_sorted")]))
+#        os.system(command)
         return num_bam
 
     def _modify_header(self, fastas):
@@ -151,34 +151,36 @@ class SNP_calling(object):
         bam_number = self._merge_bams(normal_bams, frag_bams, samtools_path, out_folder)
         seq_names = self._get_genome_name(samtools_path, out_folder)
         #### running SNP calling
-#        if depth is False:
-#            depth = -5
-#        else:
-#            depth = int(depth)
-#        if ("1" not in program ) and \
-#           ("2" not in program) and \
-#           ("3" not in program):
-#            print("Error:Please assign a correct BAQ type: '1' means 'with_BAQ', '2' means 'with_BAQ' or '3' means 'extend_BAQ'.")
-#            sys.exit()
-#        else:
-#            for fasta in os.listdir(fasta_path):
-#                if (fasta.split(".f")[0] in seq_names):
-#                    fasta_datas = self._detect_fasta(fasta)
-#                    detect = fasta_datas[0]
-#                    prefix = fasta_datas[1]
-#                    if detect:
-#                        detect = False
-#                        print("Computing " + fasta + " now ...")
-#                        out_bcf = open("tmp_bcf", "w")
-#                        self.helper.check_make_folder(os.path.join(out_folder, file_type, "SNP_table"), prefix)
-#                        out_raw_prefix = os.path.join(out_folder, file_type, "SNP_raw_outputs", prefix, prefix)
-#                        out_table_prefix = os.path.join(out_folder, file_type, "SNP_table", prefix, prefix)
-#                        fasta_file = os.path.join(fasta_path, fasta)
-#                        self._run_program(program, samtools_path, bcftools_path, fasta_file, out_bcf, 
-#                                          out_raw_prefix, out_table_prefix, quality, seq_path, prefix, 
-#                                          depth, stat_path, bam_number, fraction, out_folder)
-#                        os.remove("tmp_bcf")
-#        self.helper.remove_tmp(fastas)
-#        os.remove(os.path.join(out_folder, "whole_reads.bam"))
-#        os.remove(os.path.join(out_folder, "whole_reads_sorted.bam"))
-#        os.remove("header")
+        if depth is False:
+            depth = -5
+        else:
+            depth = int(depth)
+        if ("1" not in program ) and \
+           ("2" not in program) and \
+           ("3" not in program):
+            print("Error:Please assign a correct BAQ type: '1' means 'with_BAQ', '2' means 'with_BAQ' or '3' means 'extend_BAQ'.")
+            sys.exit()
+        else:
+            for fasta in os.listdir(fasta_path):
+                if (fasta.split(".f")[0] in seq_names):
+                    fasta_datas = self._detect_fasta(fasta)
+                    detect = fasta_datas[0]
+                    prefix = fasta_datas[1]
+                    if detect:
+                        detect = False
+                        print("Computing " + fasta + " now ...")
+                        out_bcf = open("tmp_bcf", "w")
+                        self.helper.check_make_folder(os.path.join(out_folder, file_type, "SNP_table"), prefix)
+                        self.helper.check_make_folder(os.path.join(out_folder, file_type, "SNP_raw_outputs"), prefix)
+                        out_raw_prefix = os.path.join(out_folder, file_type, "SNP_raw_outputs", prefix, prefix)
+                        out_table_prefix = os.path.join(out_folder, file_type, "SNP_table", prefix, prefix)
+                        fasta_file = os.path.join(fasta_path, fasta)
+                        table_path = os.path.join(out_folder, file_type, "SNP_table", prefix)
+                        self._run_program(program, samtools_path, bcftools_path, fasta_file, out_bcf, 
+                                          out_raw_prefix, out_table_prefix, quality, seq_path, prefix, 
+                                          depth, stat_path, bam_number, fraction, out_folder, table_path)
+                        os.remove("tmp_bcf")
+        self.helper.remove_tmp(fastas)
+        os.remove(os.path.join(out_folder, "whole_reads.bam"))
+        os.remove(os.path.join(out_folder, "whole_reads_sorted.bam"))
+        os.remove("header")
