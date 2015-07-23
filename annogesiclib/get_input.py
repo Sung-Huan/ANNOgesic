ece17fcdfebd69e76ccca62c2f0c1898ddb0e2fd
Sung-Huan Yu <sung-huan.yu@uni.wuerzburg.de>
2015-07-23 18:13:40 +0200

    change filename

--- annogesiclib/get_input.py
+++ annogesiclib/get_input.py
@@ -24,25 +24,25 @@ def get_file(ftp, input_folder, files_type):
             filename = file_[0:-2] + "fa"
             detect = True
             change = False
-        elif (file_[-3:] == "gff"):
-            with open(input_file, "r") as g_f:
-                for line in g_f:
-                    if line[0] != "#":
-                        line = line.strip()
-                        line = line.split("\t")
-                        break
-            if line[0] != file_[:-4]:
-                name = line[0]
-                os.rename(input_file, os.path.join(input_folder, name + ".gff"))
-        elif (file_[-3:] == "gbk"):
-            with open("/".join([input_folder, file_]), "r") as g_f:
-                for line in g_f:
-                    if line[0:7] == "VERSION":
-                        data = line[12:].split(" ")
-                        break
-            if data[0] != file_[:-4]:
-                name = data[0]
-                os.rename(input_file, os.path.join(input_folder, name + ".gbk"))
+#        elif (file_[-3:] == "gff"):
+#            with open(input_file, "r") as g_f:
+#                for line in g_f:
+#                    if line[0] != "#":
+#                        line = line.strip()
+#                        line = line.split("\t")
+#                        break
+#            if line[0] != file_[:-4]:
+#                name = line[0]
+#                os.rename(input_file, os.path.join(input_folder, name + ".gff"))
+#        elif (file_[-3:] == "gbk"):
+#            with open("/".join([input_folder, file_]), "r") as g_f:
+#                for line in g_f:
+#                    if line[0:7] == "VERSION":
+#                        data = line[12:].split(" ")
+#                        break
+#            if data[0] != file_[:-4]:
+#                name = data[0]
+#                os.rename(input_file, os.path.join(input_folder, name + ".gbk"))
         if detect:
             detect = False
             if change:
