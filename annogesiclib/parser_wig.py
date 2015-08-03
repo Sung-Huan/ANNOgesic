32a60dd76c702b3e1681c7ae766fcff7068d34f9
Sung-Huan Yu <sung-huan.yu@uni.wuerzburg.de>
2015-08-03 16:15:43 +0200

    fix the error of extract the header

--- annogesiclib/parser_wig.py
+++ annogesiclib/parser_wig.py
@@ -9,27 +9,29 @@ class WigParser(object):
         strain = ""
         for line in wig_fh.readlines():
             line = line.strip()
-            line = line.split(" ")
-            if (line[0] == "variableStep"):
-                strain = line[1].split("=")
+            datas = line.split(" ")
+            if (datas[0] == "variableStep"):
+                strain = datas[1].split("=")
                 strain = strain[1]
                 pre_pos = 0
                 first = True
-            if (line[0] == "track"):
-                track = line[2].split("=")
+            if (datas[0] == "track"):
+                track = datas[2].split("=")
                 track = track[1].replace("\"", "")
                 pre_pos = 0
                 first = True
-            if (line[0] != "track") and (line[0] != "variableStep"):
-                if int(line[0]) - 1 != pre_pos:
-                    for pos in range(pre_pos + 1, int(line[0])):
+            if (datas[0] != "track") and (datas[0] != "variableStep"):
+                if len(datas) != 2:
+                    datas = line.split("\t")
+                if int(datas[0]) - 1 != pre_pos:
+                    for pos in range(pre_pos + 1, int(datas[0])):
                         yield assign_value(pos, 0, strand, strain, track)
-                    pre_pos = int(line[0])
+                    pre_pos = int(datas[0])
                     first = True
-                if (int(line[0]) - 1 == pre_pos) or (first):
-                    pre_pos = int(line[0])
+                if (int(datas[0]) - 1 == pre_pos) or (first):
+                    pre_pos = int(datas[0])
                     first = False
-                    yield assign_value(line[0], line[1],
+                    yield assign_value(datas[0], datas[1],
                                        strand, strain, track)
 
 class assign_value(object):
