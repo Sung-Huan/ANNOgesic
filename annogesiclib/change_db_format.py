98df965bfd18f0354cf63831acdc2ee631325a6c
Sung-Huan Yu <sung-huan.yu@uni.wuerzburg.de>
2015-08-03 16:05:46 +0200

    add function of change sRNA db format

--- annogesiclib/change_db_format.py
+++ annogesiclib/change_db_format.py
@@ -1,19 +1,11 @@
 import os
 import sys
 import csv
-import argparse
 
-__author__ = "Sung-Huan Yu <sung-huan.yu@uni-wuerzburg.de>"
-__email__ = "sung-huan.yu@uni-wuerzburg.de"
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-i","--input_file",help="input file")
-parser.add_argument("-o","--output_file",help="output file")
-args = parser.parse_args()
-
-def main():
+def change_format(input_file, output_file):
     num = 1
-    with open(args.input_file) as f_h:
+    out = open(output_file, "w")
+    with open(input_file) as f_h:
         for line in f_h:
             line = line.strip()
             if line.startswith(">"):
@@ -21,10 +13,6 @@ def main():
                 if datas[0][1:] == "NA":
                     datas[0] = ">srn_" + str(num)
                     num += 1
-                print("|".join(datas[:3]))
+                out.write("|".join(datas[:3]) + "\n")
             else:
-                print(line)
-
-
-if __name__ == "__main__":
-    main()
+                out.write(line + "\n")
