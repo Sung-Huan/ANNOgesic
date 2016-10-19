78ac844f6101dd7066178c9eea1716c91436d72d
Sung-Huan Yu <sung-huan.yu@uni-wuerzburg.de>
2016-10-19 18:00:27 +0200

    add a script for fix name of wig

---
+++ tutorial_data/replace_seq_id.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import os
+import shutil
+import argparse
+
+__author__ = "Sung-Huan Yu <sung-huan.yu@uni-wuerzburg.de>"
+__email__ = "sung-huan.yu@uni-wuerzburg.de"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i","--input_wig_folder",help="input wig file")
+parser.add_argument("-n","--strain_name",help="strain_name")
+args = parser.parse_args()
+
+def main():
+    for wig in os.listdir(args.input_wig_folder):
+        out = open("tmp", "w")
+        with open(os.path.join(args.input_wig, wig)) as fh:
+            for line in fh:
+                if line.starswith("variableStep"):
+                    data = line.split(" ")
+                    choms = data[1].split("=")
+                    choms[-1] = args.strain_name
+                    data[1] = "=".join(choms)
+                    out.write(" ".join(data))
+                else:
+                    out.write(" ".join(data))
+        out.close()
+        os.remove(os.path.join(args.input_wig, wig))
+        shutil.move("tmp", os.path.join(args.input_wig, wig))
+
+if __name__ == "__main__":
+    main()
