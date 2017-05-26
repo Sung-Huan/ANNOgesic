63bf699da90d4e5b1074edca33d75d22414a0668
Sung-Huan Yu <sung-huan.yu@uni-wuerzburg.de>
2017-05-26 17:32:25 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='ANNOgesic',
-    version='0.6.2',
+    version='0.6.3',
     packages=['annogesiclib'],
     author='Sung-Huan Yu',
     author_email='sung-huan.yu@uni-wuerzburg.de',
