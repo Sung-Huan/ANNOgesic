4d664283d1c6dbf94029946a3f01cace0b05eab5
Sung-Huan Yu <sung-huan.yu@uni-wuerzburg.de>
2017-07-10 18:06:19 +0200

    update interface of annotation transfer

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='ANNOgesic',
-    version='0.6.9',
+    version='0.6.10',
     packages=['annogesiclib'],
     author='Sung-Huan Yu',
     author_email='sung-huan.yu@uni-wuerzburg.de',
