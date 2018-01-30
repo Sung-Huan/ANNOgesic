7098f68d1b236fa79141fb9852ea232c80813538
Sung-Huan Yu <sung-huan.yu@uni-wuerzburg.de>
2018-01-30 15:33:20 +0100

    update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst')) as f:
 
 setup(
     name='ANNOgesic',
-    version='0.7.5',
+    version='0.7.6',
     packages=['annogesiclib'],
     author='Sung-Huan Yu',
     author_email='sung-huan.yu@uni-wuerzburg.de',
