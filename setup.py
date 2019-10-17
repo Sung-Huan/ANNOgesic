48273d8d70b080610d9492ac2a3425bed4942a27
pgsin <pgsinitsyn@gmail.com>
2019-10-17 14:57:38 +0200

    fix Pypi

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst')) as f:
 
 setup(
     name='ANNOgesic',
-    version='1.0.7',
+    version='1.0.8',
     packages=['annogesiclib'],
     author='Sung-Huan Yu',
     author_email='sung-huan.yu@uni-wuerzburg.de',
